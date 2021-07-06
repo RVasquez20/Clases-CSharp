844c995b814f78c4498d058b77222735243c517d
Oscher15 <Alexanderbarrios15@outlook.com>
2021-07-06 10:06:12 -0600

    Tarea 6

---
+++ C#/Tareas/Tarea_6/Tarea_6/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace Tarea_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion = 0, dias = 0, anios = 0, meses = 0, semanas = 0;
+            int diasrestantes=0;
+            int[] nota = new int[10];
+            int[] numero1 = new int[10];
+            int[] numero2 = new int[10];
+            int[] suma = new int[10];
+            int[] resta = new int[10];
+            int[] multiplicacion = new int[10];
+            int[] division = new int[10];
+            int[] resultante = new int[10];
+            string usuario;
+          
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(" Seleccione la opción que desea realizar");
+                Console.WriteLine(" 1)  Nota mayor");
+                Console.WriteLine(" 2)  Operaciones básicas");
+                Console.WriteLine(" 3)  Resta sin el operador -");
+                Console.WriteLine(" 4)  Determinar años, meses, días");
+                Console.WriteLine(" 5)  Promedio sueldo anual");
+                opcion = int.Parse(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                        {
+
+                            for (int i = 0; i < 10; i++)
+                            {
+                                Console.WriteLine("Ingrese la nota");
+                                nota[i] = int.Parse(Console.ReadLine());
+                                Array.Sort(nota);
+                                
+                            }
+                            Console.Write("La nota mayor es:  "+nota[9]);
+
+                            break;
+                        }
+                    case 2:
+                        {
+                            for (int j = 0; j < 10; j++)
+                            {
+                                Console.WriteLine("Ingrese un número");
+                                numero1[j] = int.Parse(Console.ReadLine());
+                            }
+                            for (int j = 0; j < 10; j++)
+                            {
+                                Console.WriteLine("Ingrese un número");
+                                numero2[j] = int.Parse(Console.ReadLine());
+                            }
+                            for (int op = 0; op < 10; op++)
+                            {
+                                suma[op] = numero1[op] + numero2[op];
+                                resta[op] = numero1[op] - numero2[op];
+                                multiplicacion[op] = numero1[op] * numero2[op];
+                                division[op] = numero1[op] / numero2[op];
+                                resultante[op] = suma[op] * resta[op] * multiplicacion[op] * division[op];
+                                Console.WriteLine("el resultado de la multiplicación será: " + resultante[op]);
+                            }
+                            
+                            
+                            break;
+                        }
+                    case 3:
+                        {
+                            /*a=6;
+                              b=2+1=3+1=4+1=5+1=6;
+                              res=0/1/2/3/4;
+                            */
+                            int a = 6, b = 2, res=0;
+                            for (res = 0; b < a; res++) b++;
+
+                            Console.WriteLine(res);
+
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.WriteLine("Ingrese el número de días");
+                            dias = int.Parse(Console.ReadLine());
+                           
+                                anios = dias / 365;
+                               
+                               
+                                meses = dias / 30;
+                                
+                                semanas = dias / 7;
+                                
+
+                            
+                            Console.WriteLine("Los años son: " + anios);
+                            Console.WriteLine("Los años son: " + meses);
+                            Console.WriteLine("Los años son: " + semanas);
+                            Console.WriteLine("Los años son: " + dias);
+                            break;
+                        }
+                    case 5:
+                        {
+
+                            break;
+                        }
+                    default:
+                        {
+                            break;
+                        }
+
+                }
+                Console.WriteLine("¿Desea continuar?");
+                usuario = Console.ReadLine();
+            }
+            while (usuario.Equals("Si"));
+            {
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
