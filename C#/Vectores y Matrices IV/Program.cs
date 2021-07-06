75c69b5bf0521ecb05770bb2be64bbf1ddfe5213
Snowden <55126792+Snowdenv20@users.noreply.github.com>
2021-07-06 15:18:26 -0600

    Vectores Y Matricez IV

---
+++ C#/Vectores y Matrices IV/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Vectores_y_Matrices_IV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2 ejercicios de duda
+            /*
+                        int dias=0,anios=0,meses=0,semanas=0;
+                        Console.WriteLine("Ingrese la cantidad de dias");
+                        dias = int.Parse(Console.ReadLine());
+
+                            anios = dias / 365;
+                            meses = dias / 30;
+                        semanas = dias / 7;
+                        Console.WriteLine("Los anios son:"+anios);
+                        Console.WriteLine("Los meses son:" + meses);
+                        Console.WriteLine("Los semanas son:" + semanas);
+                        Console.WriteLine("Los dias son:" + dias);
+                        */
+
+            /*
+            a=6;
+            b=2/3/4/5/6;
+            res=0/1/2/3/4;
+             */
+            /*
+            int a = 6, b = 2, res = 0;
+            for (res = 0; b < a; res++) { 
+                b++;
+        }
+            Console.WriteLine(res);
+                    
+            */
+            /*Matrices:
+             Una matriz es una estructura de datos que permite almacenar un conjunto de datos del mismo tipo.
+
+            Con un unico nombre se define la matriz y por medio de DOS sub indices hacemos referencia a 
+            cada elemento de la misma
+             */
+            Console.ReadKey();
+        }
+    }
+}
