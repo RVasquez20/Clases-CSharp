425ce03f4adf3dd1ae99acf0271ccaee9a1a93b5
Snowden <55126792+Snowdenv20@users.noreply.github.com>
2021-07-27 13:42:13 -0600

    Interfaces

---
+++ C#/Clases IV/Plantas.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_IV
+{
+    class Plantas
+    {
+        /*Modificadores de acceso(Getters and Setters):
+         Getters=devuelven el valor de un atributo
+         Setters=dan un valor a un atributo
+        */
+        private string  tipo;
+        private bool frutos;
+        private int cantidadDeFrutos;
+        /*Ejemplo de getter antiguo
+        public string getColor()
+        {
+            return color;
+        }
+        Ejemplo de setter antiguo
+        public void setColor(string color)
+        {
+            this.color = color;
+        }*/
+        /*
+         Si se declara el setter and getter ya no es necesario colocar
+        el atributo
+         */
+        public string color { get; set; }
+        public Plantas() {}
+
+        public Plantas(string color,string tipo,bool frutos)
+        {
+            this.color = color;
+            this.tipo = tipo;
+            this.frutos = frutos;
+            this.cantidadDeFrutos = 0;
+        }
+
+        public Plantas(string color, string tipo, bool frutos,int cantidadDeFrutos)
+        {
+            this.color = color;
+            this.tipo = tipo;
+            this.frutos = frutos;
+            this.cantidadDeFrutos = cantidadDeFrutos;
+        }
+        /*
+         Sobrecarga de metodos:
+        pueden existir metodos del mismo tipo, del mismo nombre, pero su diferencia
+        sera la cantidad de parametros
+         */
+        public void MostrarDatos()
+        {
+            Console.WriteLine($"Color:{color}");
+            Console.WriteLine("Tipo:{0}",tipo);
+            Console.WriteLine("Frutos:"+frutos);
+            Console.WriteLine("Cantidad:"+cantidadDeFrutos);
+        }
+        public void MostrarDatos(int cantidad)
+        {
+            Console.WriteLine($"Color:{color}");
+            Console.WriteLine("Tipo:{0}", tipo);
+            Console.WriteLine("Frutos:" + frutos);
+            Console.WriteLine("Cantidad:" + cantidad);
+        }
+    }
+}
---
+++ C#/FormsI/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace FormsI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Prueba";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Info;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(600, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 450);
+            this.panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(35, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 59);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Hola Mundo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
